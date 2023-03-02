bd0902fe06bc560614ed855ae4fd3f818dc6e957
Nolan Martin <nmartinsdtc@gmail.com>
2023-03-02 14:37:08 -0700

    added space between variables and print line

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -10,6 +10,7 @@
             bool isHappy = true;
             double priceOfEggs = 6.25;
             decimal priceOfBeef = 4.50m;
+
             Console.WriteLine($"{longWord}, {letter} {reallyBigNumber} pack of eggs costs ${priceOfEggs}, while " +
                 $"a pound of beef costs ${priceOfBeef}. I can't believe that's {isHappy}!");
         }
